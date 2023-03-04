e867fd1c371138ab7b8c6c3cb2aed162ef92d0f8
adamdavala <adamdavana7@gmail.com>
2023-03-04 13:03:11 +0700

    initial commit

---
+++ resources/views/dashboard/about.blade.php
@@ -0,0 +1,17 @@
+@extends('../layouts.mainlayout')
+
+@section('title', 'About')
+
+@section('content')
+<div class="card mx-auto mt-3" style="width: 18rem;">
+  <img src="https://avatars.githubusercontent.com/u/12584890?v=4" class="card-img-top" alt="...">
+  <div class="card-body bg-dark text-light">
+    <h5 class="card-title">Adam Kece Badai sangat</h5>
+    <p class="card-text">Adam yang selalu bekerja keras demi apa yang dia dapatkan dan terus berdoa.</p>
+  </div>
+  <div class="card-body bg-dark text-light">
+    <a href="#" class="card-link">Instagram</a>
+    <a href="#" class="card-link">Facebook</a>
+  </div>
+</div>
+@endsection
---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,8 @@
+@extends('../layouts.mainlayout')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1 class="text-center mt-5 text-light">Sebab kau terlaliu indah dari sekedar kata</h1>
+    <h3 class="text-center mt-5 text-light">Dunia berhenti sejenak menikmati indahmu</h3>
+@endsection
